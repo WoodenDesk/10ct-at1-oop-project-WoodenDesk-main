b477a0ab8a90c75aa440b4c53bb4d8f510256656
WoodenDesk <Veres_be@student.kings.edu.au>
2025-02-24 12:34:25 +1100

    Player half way done

--- player.py
+++ player.py
@@ -3,39 +3,47 @@ import app  # Contains global settings like WIDTH, HEIGHT, PLAYER_SPEED, etc.
 
 class Player:
     def __init__(self, x, y, assets):
-        """Initialize the player with position and image assets."""
-        # TODO: 1. Store the player's position
-        # e.g. self.x = x, self.y = y
-
-        # TODO: 2. Load the player's image from assets
-        # For example: self.image = assets["player_idle"][0]
-        # (or some default image key in assets)
-
+        self.x = x
+        self.y = y
         self.speed = app.PLAYER_SPEED
+
         self.animations = assets["player"]
         self.state = "idle"
         self.frame_index = 0
         self.animation_timer = 0
         self.animation_speed = 8
 
-        # TODO: 3. Create a collision rectangle (self.rect) 
-        # For example: 
-        # self.rect = self.image.get_rect(center=(self.x, self.y))
+        self.image = self.animations[self.state][self.frame_index]
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+        self.facing_left = False
 
-        # TODO: 4. Add player health 
+        self.health = 5
+ 
+
+ 
 
     def handle_input(self):
         """Check and respond to keyboard/mouse input."""
 
         # TODO: 1. Capture Keyboard Input
-
+        keys = pygame.key.get_pressed() #Get the key press event
         # velocity in X, Y direction
         vel_x, vel_y = 0, 0
 
         # TODO: 2. Adjust player position with keys pressed, updating the player position to vel_x and vel_y
+        if keys[pygame.K_LEFT]:
+            # Move character left
+            vel_x -= self.speed
+        if keys[pygame.K_RIGHT]:
+            vel_x += self.speed
+        if keys[pygame.K_UP]:
+            vel_y -= self.speed
+        if keys[pygame.K_DOWN]:
+            vel_y += self.speed
 
         # TODO: 3. Clamp player position to screen bounds
-
+        self.x = min(max(self.x + vel_x, 0 ),app.WIDTH) #set the minimum of the players x axis to their current x position + 
+        self.y = min(max(self.y + vel_y, 0 ),app.HEIGHT)# set the players minimum y position to their current y position + their velocity 
         # animation state
         if vel_x != 0 or vel_y != 0:
             self.state = "run"
@@ -47,9 +55,11 @@ class Player:
             self.facing_left = True
         elif vel_x > 0:
             self.facing_left = False
-        pass
+
 
     def update(self):
+        self.update_animation()
+    def update_animation(self):
         self.animation_timer += 1
         if self.animation_timer >= self.animation_speed:
             self.animation_timer = 0
@@ -59,7 +69,7 @@ class Player:
             center = self.rect.center
             self.rect = self.image.get_rect()
             self.rect.center = center
-        pass
+
 
     def draw(self, surface):
         """Draw the player on the screen."""
